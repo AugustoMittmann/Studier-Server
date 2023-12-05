1fd91831f096495b2493f2583a93e400d3114452
Augusto Mittmann <gutomittmann10@gmail.com>
2023-12-05 18:02:55 -0300

    agora funciona o back

--- src/server.js
+++ src/server.js
@@ -1,39 +1,35 @@
 import express from "express";
 import { create } from "./bot.js";
 import { PrismaClient } from "@prisma/client";
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 const prisma = new PrismaClient();
 
+
 app.get('/users', async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   const users = await prisma.user.findMany();
   res.send(users)
 })
 
-app.post('/create', async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  const {name, password} = req.body;
-
-  await prisma.user.create({
-    data: {
-      name: 'name',
-      password: 'password'
-    }
-  })
+app.get('/create', async (req, res) => {
+  try {
+    await prisma.user.create({
+      data: {
+        name: req.query.name,
+        password: req.query.password
+      }
+    })
+  } catch (e) {
+    console.log(e);
+  }
 
   return res.status(201).send()
 })
 
 app.get('/test', async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   try {
     const data = await create(req.query.content);
     res.send(data);
