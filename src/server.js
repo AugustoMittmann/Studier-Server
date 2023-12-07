0bf1f4cf58f61612a281427c025305756d5e37ca
Augusto Mittmann <gutomittmann10@gmail.com>
2023-12-07 17:57:57 -0300

    insert new column content

--- src/server.js
+++ src/server.js
@@ -63,4 +63,108 @@ app.get('/question', async (req, res) => {
   }
 })
 
+app.get('/saveHistory', async (req, res) => {
+  try{
+    let questions = [];
+    let rightAnswers = [];
+    let userAnswers = [];
+    //console.log(req.query)
+    req.query.history.forEach(data => {
+      questions.push(data.question ? data.question : "");
+      rightAnswers.push(data.rightAnswer ? data.rightAnswer : "");
+      userAnswers.push(data.userAnswer ? data.userAnswer : "");
+    })
+    const userId = req.query.userId
+    const historyDB = await prisma.history.create({
+      data: {
+        userId,
+        finalGrade: parseInt(req.query.finalGrade),
+      }
+    })
+    const questionsDB = await prisma.questions.create({
+      data: {
+        question1: questions[0],
+        question2: questions[1],
+        question3: questions[2],
+        question4: questions[3],
+        question5: questions[4],
+        question6: questions[5],
+        question7: questions[6],
+        question8: questions[7],
+        question9: questions[8],
+        question10: questions[9],
+        historyId: historyDB.id
+      }
+    })
+    const rightAnswersDB = await prisma.rightAnswers.create({
+      data: {
+        answer1: rightAnswers[0],
+        answer2: rightAnswers[1],
+        answer3: rightAnswers[2],
+        answer4: rightAnswers[3],
+        answer5: rightAnswers[4],
+        answer6: rightAnswers[5],
+        answer7: rightAnswers[6],
+        answer8: rightAnswers[7],
+        answer9: rightAnswers[8],
+        answer10: rightAnswers[9],
+        historyId: historyDB.id
+      }
+    })
+    const userAnswersDB = await prisma.userAnswers.create({
+      data: {
+        answer1: userAnswers[0],
+        answer2: userAnswers[1],
+        answer3: userAnswers[2],
+        answer4: userAnswers[3],
+        answer5: userAnswers[4],
+        answer6: userAnswers[5],
+        answer7: userAnswers[6],
+        answer8: userAnswers[7],
+        answer9: userAnswers[8],
+        answer10: userAnswers[9],
+        historyId: historyDB.id
+      }
+    })    
+  } catch (e){
+    console.log(e)
+  }
+})
+
+app.get('/showHistory', async (req, res) => {
+  const historyDB = await prisma.history.findMany({
+    where: {
+      userId: req.query.userId,
+    }
+  });
+  const id = historyDB.map((history) => history.id);
+
+  const getQuestion = await prisma.questions.findMany({
+    where: {
+      historyId: { in: id }
+    }
+  })
+  const getUserAnswer = await prisma.userAnswers.findMany({
+    where: {
+      historyId: { in: id }
+    }
+  })
+  const getRightAnswer = await prisma.rightAnswers.findMany({
+    where: {
+      historyId: { in: id }
+    }
+  })
+  const result = []
+  for(let i = 0; i< getQuestion.length; i++) {
+    result.push({
+      getQuestion: getQuestion[i],
+      getUserAnswer: getUserAnswer[i],
+      getRightAnswer: getRightAnswer[i],
+      finalGrade: historyDB[i].finalGrade,
+    })
+  }
+
+  res.send(result)
+})
+
 app.listen(4000);
